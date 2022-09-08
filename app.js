eadfd4e29ff39e4d08b4f9e6d7d1d7234caa3551
Divine Favour <favourme2580@gmail.com>
2022-09-08 16:11:53 +0100

    Check BP for reference

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const methodOverride = require('method-override');
-const morgan = require('morgan');
+const ejsMate = require('ejs-mate');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 
@@ -15,14 +15,15 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true,
 
 const app = express();
 
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.use(morgan('tiny'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
+    console.log(req.reqTime)
     res.render('home')
 })
 
